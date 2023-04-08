3d018618cc1b20dbe4542494667bb2080af196bc
Darguima <dsgdevbraga@gmail.com>
2023-04-08 11:42:52 +0100

    Lightdm - new theme (with webkit2 greeter)

--- InstallationScripts/lightdm.py
+++ InstallationScripts/lightdm.py
@@ -1,12 +1,13 @@
 from Utils.print_log import print_log_box, print_log_status
 from Utils.install_package import install_package
 from Utils.create_sym_link import create_sym_link
+from Utils.copy import copy
 from Utils.systemctl import systemctl
 
 def installAndConfigure(CONSTANTS: dict, args: dict):
   print_log_box("lightdm")
 
-  install_package("lightdm", ["lightdm-gtk-greeter"])
+  install_package("lightdm", ["lightdm-webkit2-greeter"])
 
   systemctl.enable("lightdm")
 
@@ -14,6 +15,11 @@ def installAndConfigure(CONSTANTS: dict, args: dict):
 
   print_log_status(3, "Linking `lightdm.conf`")
   create_sym_link(f"{CONSTANTS['DOTFILES']}/dotfiles/lightdm/lightdm.conf", f"/etc/lightdm", "lightdm.conf", sudo=True)
+  
+  # In this case is not possible to link the files, it's needed to copy them
+  print_log_status(3, "Copying `lightdm-webkit2-greeter.conf` and `rui_theme` folder")
+  copy(f"{CONSTANTS['DOTFILES']}/dotfiles/lightdm/lightdm-webkit2-greeter.conf", f"/etc/lightdm/lightdm-webkit2-greeter.conf", sudo=True)
+  copy(f"{CONSTANTS['DOTFILES']}/dotfiles/lightdm/rui_theme", f"/usr/share/lightdm-webkit/themes/rui_theme", sudo=True)
   
   print_log_status(4)
 
