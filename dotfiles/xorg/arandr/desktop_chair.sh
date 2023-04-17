269c69033d816294f245a3406908513b7718ab87
Darguima <dsgdevbraga@gmail.com>
2023-04-17 01:25:55 +0100

    New monitors config

--- dotfiles/xorg/arandr/desktop_chair.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate inverted --output HDMI-0 --off
--- dotfiles/xorg/arandr/desktop_default.sh
+++ dotfiles/xorg/arandr/desktop_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x1080 --rotate inverted --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate inverted --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
